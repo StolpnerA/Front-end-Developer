25ca26c8a12c7e0756142c094967a6ec8e43da21
AndreyS <andreysx@tut.by>
2017-02-12 16:48:33 +0300

    Project 02-12

--- Project/js/jQuery.js
+++ Project/js/jQuery.js
@@ -4,10 +4,19 @@
 
 $(function () {
     $('#fS').click(function () {
-        $('form').slideToggle('slow');
+        $('form').slideToggle('1500');
     });
 
     $('#cancel').click(function () {
-        $('form').slideUp('slow');
+        $('form').slideUp('1500');
     });
+    $('article').css('display', 'none');
+    $('article').fadeIn(1200);
+    $('.ddImg').css('text-align', 'left')
+    $('article img').css({
+        'width': '0'
+    });
+    $('article img').animate({
+        'width': '200px'
+    }, 500);
 });
